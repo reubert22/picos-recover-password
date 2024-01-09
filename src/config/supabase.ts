c02517c3f0d505a905755de3c90e4e06f7aadb96
reubert22 <reubertfernandes@gmail.com>
2024-01-09 01:15:20 -0300

    fix: remove unused import type

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "@/lib/database.types";
 
-export const supabaseClient = createClient<Database>(
+export const supabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
