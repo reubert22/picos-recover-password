69bc5e51c8c1625cf5483927b36d5bb2f2b94de0
reubert22 <reubertfernandes@gmail.com>
2024-01-09 01:49:15 -0300

    fix: make it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,14 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center  p-24">
+    <main className="flex min-h-screen flex-col items-center p-4 md:p-8 lg:p-16 xl:p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="flex items-end">
-          <p className="relative dark:drop-shadow-[0_0_0.3rem_#000] text-white text-5xl">
+          <p className="relative dark:drop-shadow-[0_0_0.3rem_#000] text-white text-3xl md:text-5xl">
             Picos
           </p>
         </div>
       </div>
-      <p className="relative dark:drop-shadow-[0_0_0.3rem_#000] text-white text-xl"></p>
 
       <PasswordRecoveryForm />
     </main>
