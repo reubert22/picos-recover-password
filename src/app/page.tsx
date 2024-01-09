ac526bcf0d77f6b3fd22cc4311c799ad59880f9b
reubert22 <reubertfernandes@gmail.com>
2024-01-09 00:24:33 -0300

    feat: add config to supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
 "use client";
 
+import React, { useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@/lib/database.types";
+
 export default function Home() {
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  );
+
+  async function getApprovedCategories() {
+    try {
+      const { data: category, error } = await supabase
+        .from("category")
+        .select("*")
+        .eq("status", true);
+
+      console.log(category);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getApprovedCategories();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -17,7 +43,6 @@ export default function Home() {
   );
 }
 
-import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
